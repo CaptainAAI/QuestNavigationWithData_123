3fc7e936e37aa7e0a5ed770ff07b73fe2c57be2b
Ananda Agung Ismail <158981471+CaptainAAI@users.noreply.github.com>
2025-08-19 08:38:36 +0000

    fix dekalrasi

--- app/src/main/java/com/example/dependensimvvm/view/Formulir.kt
+++ app/src/main/java/com/example/dependensimvvm/view/Formulir.kt
@@ -22,6 +22,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -34,10 +35,10 @@ import com.example.dependensimvvm.R
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FormIsian(pilihanJK: List<String> , onSubmitBtnClick: () -> Unit) {
-    val jenisK = listOf("Laki-laki", "Perempuan")
-    var namaLengkap by remember { mutableStateOf("") }
-    var alamat by remember { mutableStateOf("") }
-    val listData: MutableList<String> = mutableListOf(txtNama,txtGender,txtAlamat) }
+    var txtNama by rememberSaveable { mutableStateOf("") }
+    var txtAlamat by remember { mutableStateOf("") }
+    var txtGender by remember { mutableStateOf("") }
+    val listData: MutableList<String> = mutableListOf(txtNama,txtGender,txtAlamat)
 
     Scaffold(
         topBar = {
